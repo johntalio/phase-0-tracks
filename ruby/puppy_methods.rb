27fa56389b351ac15bd2e8e55d0f0c3b305c3800
John Talio <johntalio@yahoo.com>
2016-10-18 14:26:24 -0400

    added roll over method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -8,8 +8,14 @@ class Puppy
   def speak(int)
   	puts "Woof "*int
   end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
 end
 puppy = Puppy.new
 puppy.fetch("ball")
 
 puppy.speak(3)
+
+puppy.roll_over
