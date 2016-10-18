a7d0b5301275d6ffe77591165d8595cfc0b8914c
John Talio <johntalio@yahoo.com>
2016-10-18 14:30:20 -0400

    added eat method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -16,6 +16,10 @@ class Puppy
   def dog_years(int)
   	p int*7
   end
+
+  def eat(item)
+  	puts "Yummy, #{item}!"
+  end
 end
 puppy = Puppy.new
 puppy.fetch("ball")
@@ -26,5 +30,5 @@ puppy.roll_over
 
 puppy.dog_years(3)
 
-
+puppy.eat("newspaper")
 
