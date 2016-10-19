8e8fdd3960725956cfc850cd5a391341c84de56e
John Talio <johntalio@yahoo.com>
2016-10-19 17:59:50 -0400

    Completed Bird and Plane classes

--- ruby/flight.rb
+++ ruby/flight.rb
@@ -1,8 +1,20 @@
-
+module Flight
+  def take_off(altitude)
+    puts "Taking off, and ascending until we've reached #{altitude} feet..."
+  end
+end
 
 
 class Bird
+	include Flight
 end
 
 class Plane
+	include Flight
 end
+
+bird = Bird.new
+bird.take_off(800)
+
+plane = Plane.new
+plane.take_off(30000)
