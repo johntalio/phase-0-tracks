d4af147369de9b717edd51a42478818684446638
John Talio <johntalio@yahoo.com>
2016-10-19 18:16:01 -0400

    not done

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,30 @@
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
-
-  def self.yell_happily(words)
-  	words + "!*!*!*!*" + ":^D"
-  end
+	def yell_hopelessly(words)
+		puts words + "!?!?!?!?!?" + ">:O"
+	end
+#  def self.yell_angrily(words)
+#    words + "!!!" + " :("
+#  end
+#
+#  def self.yell_happily(words)
+#  	words + "!*!*!*!*" + ":^D"
+#  end
 end
 
-puts Shout.yell_angrily("DAGNABBIT")
-puts Shout.yell_happily("HOORAY")
+#puts Shout.yell_angrily("DAGNABBIT")
+#puts Shout.yell_happily("HOORAY")
+
+class Existential_Meltdown
+	include Shout
+end
+
+class Amnesia
+	include Shout
+end
+
+existential = Existential_Meltdown.new
+existential.yell_hopelessly("WHO AM I")
+
+amnesia = Amnesia.new
+amnesia.yell_hopelessly("WHO AM I")
+
