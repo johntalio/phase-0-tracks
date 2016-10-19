f0b0e60af670e9d46020bb801c317bf8e1e4b43d
John Talio <johntalio@yahoo.com>
2016-10-19 14:12:38 -0400

    Updated Santa class with attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,12 @@
 class Santa
+	def initialize(gender, ethnicity)
+		puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+	end
+
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
@@ -7,10 +15,20 @@ class Santa
 		puts "That was a good #{string}!"
 	end
 
+	def about
+		puts "Gender = #{@gender}"
+		puts "Ethnicity = #{@ethnicity}"
+		puts "Reindeer Ranking = #{@reindeer_ranking}"
+		puts "Age = #{@age}"
+	end
+
 end
 
-santa = Santa.new
+santa = Santa.new("Female", "African American")
 
-puts santa.speak
+santa.about
 
-puts santa.eat_milk_and_cookies("chocolate chip")
+santa.speak
+
+santa.eat_milk_and_cookies("chocolate chip")
+
