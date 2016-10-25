68263d99bb77f83d93b43264c896ff755e82328e
John Talio <johntalio@yahoo.com>
2016-10-25 19:28:02 -0400

    Add a horse name and a color

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+var colors = ["red", "blue", "white", "black"];
+
+var horse = ["Ed", "Jack", "Seabiscuit", "Seahorse"];
+
+colors.push("orange");
+horse.push("Bob");
+
+console.log(horse);
+console.log(colors)
