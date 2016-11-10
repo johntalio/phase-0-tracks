de4b2a9afa2e403d8ed91b67af11791234b52736
John Talio <johntalio@yahoo.com>
2016-11-10 15:16:50 -0500

    Complete html site with JS File and Event Listeners

---
+++ web_dev/client_side_js/script.js
@@ -0,0 +1,38 @@
+console.log("This works!");
+
+function undermine(event) {
+	console.log("You clicked.");
+	console.log(event);
+	event.target.style = "text-transform:lowercase;";
+}
+
+function addRedBorder(event) {
+	console.log("You clicked.");
+	console.log(event);
+	event.target.style.border = "10px solid red";
+}
+
+function hide(event) {
+	console.log("You clicked.");
+	console.log(event);
+	event.target.style.visibility = "hidden";
+}
+
+function emphasisMajor(event) {
+	console.log("You clicked.");
+	console.log(event);
+	event.target.style = "font-weight:800;";
+	event.target.style.border = "10px solid pink";
+}
+
+var header = document.getElementById("small");
+header.addEventListener("click", undermine);
+
+var photo = document.getElementById("oh-hello-photo");
+photo.addEventListener("click", addRedBorder);
+
+var list = document.getElementById("bye-bye");
+list.addEventListener("click", hide);
+
+var word = document.getElementById("big");
+word.addEventListener("click", emphasisMajor);
